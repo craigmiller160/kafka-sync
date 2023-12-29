808f59cbedb0c031c450dd93b01bb320da69bf6c
Craig Miller <craigmiller160@gmail.com>
2023-12-29 10:32:14 -0500

    Progress

--- producer/src/main/java/io/craigmiller160/kafka/sync/producer/PeopleController.java
+++ producer/src/main/java/io/craigmiller160/kafka/sync/producer/PeopleController.java
@@ -1,12 +1,30 @@
 package io.craigmiller160.kafka.sync.producer;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/producer/people")
 @RequiredArgsConstructor
 public class PeopleController {
+    private final Database database;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void addPerson(@RequestBody final Person person) {
+        database.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getPeople() {
+        return database.getPeople();
+    }
 }
