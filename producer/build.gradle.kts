308ae4d3989a594ea8d20093ee843e1a9152f43f
Craig Miller <craigmiller160@gmail.com>
2023-12-29 10:17:27 -0500

    Setting up gradle settings for project

---
+++ producer/build.gradle.kts
@@ -0,0 +1,16 @@
+val projectGroup: String by project
+val projectVersion: String by project
+
+plugins {
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = projectGroup
+version = projectVersion
+java.sourceCompatibility = JavaVersion.VERSION_21
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.kafka:spring-kafka")
+}
