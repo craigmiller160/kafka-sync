ea1d85ba911718e6c02ae17d966b2a986ca7ff16
Craig Miller <craigmiller160@gmail.com>
2023-12-30 09:59:22 -0500

    Progress

---
+++ consumer/src/main/java/io/craigmiller160/kafka/sync/consumer/controller/PeopleController.java
@@ -0,0 +1,22 @@
+package io.craigmiller160.kafka.sync.consumer.controller;
+
+import io.craigmiller160.kafka.sync.consumer.database.Database;
+import io.craigmiller160.kafka.sync.consumer.dto.Person;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/consumer/people")
+@RequiredArgsConstructor
+public class PeopleController {
+    private final Database database;
+    public List<Person> getPeople() {
+        log.info("Getting people");
+        return database.getPeople();
+    }
+}
