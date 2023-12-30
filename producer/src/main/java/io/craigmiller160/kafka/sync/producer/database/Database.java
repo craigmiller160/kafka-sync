3fa1f53a13bf8cebe9986288305187fbe39bf577
Craig Miller <craigmiller160@gmail.com>
2023-12-30 10:02:14 -0500

    Setting up projects to load data from file

--- producer/src/main/java/io/craigmiller160/kafka/sync/producer/database/Database.java
+++ producer/src/main/java/io/craigmiller160/kafka/sync/producer/database/Database.java
@@ -1,12 +1,15 @@
 package io.craigmiller160.kafka.sync.producer.database;
 
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.craigmiller160.kafka.sync.producer.dto.Person;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -18,6 +21,19 @@ public class Database {
     private static final File DATABASE_FILE = Paths.get(System.getProperty("user.dir"), "database.json").toFile();
     private final ObjectMapper objectMapper;
     private final List<Person> people = new ArrayList<>();
+
+    @SneakyThrows
+    @PostConstruct
+    public synchronized void loadDataIfExists() {
+        if (DATABASE_FILE.exists()) {
+            final JavaType peopleListType = objectMapper.getTypeFactory().constructCollectionType(List.class, Person.class);
+            try (final var inputStream = new FileInputStream(DATABASE_FILE)) {
+                final List<Person> fileData = objectMapper.readValue(inputStream, peopleListType);
+                people.clear();
+                people.addAll(fileData);
+            }
+        }
+    }
 
     @SneakyThrows
     public synchronized void addPerson(final Person person) {
