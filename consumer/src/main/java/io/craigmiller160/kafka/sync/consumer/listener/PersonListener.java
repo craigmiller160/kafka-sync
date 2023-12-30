377fc5277a07c5539422c091b00ff97973a1cb0e
Craig Miller <craigmiller160@gmail.com>
2023-12-30 12:22:12 -0500

    Updated code

--- consumer/src/main/java/io/craigmiller160/kafka/sync/consumer/listener/PersonListener.java
+++ consumer/src/main/java/io/craigmiller160/kafka/sync/consumer/listener/PersonListener.java
@@ -1,15 +1,21 @@
 package io.craigmiller160.kafka.sync.consumer.listener;
 
+import io.craigmiller160.kafka.sync.consumer.database.Database;
 import io.craigmiller160.kafka.sync.consumer.dto.Person;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class PersonListener {
+    private final Database database;
+
     @KafkaListener(topics = "person-topic")
     public void addPerson(final Person person) {
         log.info("Received person {}", person);
+        database.addPerson(person);
     }
 }
