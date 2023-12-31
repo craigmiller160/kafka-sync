55a838f4b6915d73ca922eef8e70433391fabe36
Craig Miller <craigmiller160@gmail.com>
2023-12-31 18:11:18 -0500

    Trying to make it accept the cert

--- consumer/src/main/java/io/craigmiller160/kafka/sync/consumer/Runner.java
+++ consumer/src/main/java/io/craigmiller160/kafka/sync/consumer/Runner.java
@@ -4,6 +4,8 @@ import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.net.ssl.HttpsURLConnection;
+
 @SpringBootApplication
 public class Runner {
     @SneakyThrows
@@ -11,6 +13,7 @@ public class Runner {
         final var truststore = Runner.class.getClassLoader().getResource("truststore.jks").toURI().getPath();
         System.setProperty("javax.net.ssl.trustStore", truststore);
         System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
         SpringApplication.run(Runner.class, args);
     }
 }
