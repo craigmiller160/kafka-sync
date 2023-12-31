8ef8d0808881edda08ae5707c824515fd32b4346
Craig Miller <craigmiller160@gmail.com>
2023-12-31 18:09:24 -0500

    Updating truststore

--- consumer/src/main/java/io/craigmiller160/kafka/sync/consumer/Runner.java
+++ consumer/src/main/java/io/craigmiller160/kafka/sync/consumer/Runner.java
@@ -1,11 +1,16 @@
 package io.craigmiller160.kafka.sync.consumer;
 
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Runner {
+    @SneakyThrows
     public static void main(final String[] args) {
+        final var truststore = Runner.class.getClassLoader().getResource("truststore.jks").toURI().getPath();
+        System.setProperty("javax.net.ssl.trustStore", truststore);
+        System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
         SpringApplication.run(Runner.class, args);
     }
 }
