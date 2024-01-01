b588a6a2e658bee59229f6f1ceca270ae38d21d9
Craig Miller <craigmiller160@gmail.com>
2024-01-01 16:30:16 -0500

    Fixing acl settings

--- terraform/acls.tf
+++ terraform/acls.tf
@@ -16,11 +16,20 @@ resource "kafka_acl" "consumer_user_person_topic_acl" {
   resource_type       = "Topic"
 }
 
+resource "kafka_acl" "consumer_user_person_topic_group_acl" {
+  acl_host            = "*"
+  acl_operation       = "Read"
+  acl_permission_type = "Allow"
+  acl_principal       = "User:consumer_user"
+  resource_name       = "person-topic-group"
+  resource_type       = "Group"
+}
+
 resource "kafka_acl" "consumer_user_person_topic_dlt_acl" {
   acl_host            = "*"
   acl_operation       = "Write"
   acl_permission_type = "Allow"
   acl_principal       = "User:consumer_user"
-  resource_name       = "person-topic"
+  resource_name       = "person-topic-dlt"
   resource_type       = "Topic"
 }
