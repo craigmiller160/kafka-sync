4a63880229f853c9411db3b7de512b9d7f6c7f52
Craig Miller <craigmiller160@gmail.com>
2024-01-01 11:29:27 -0500

    Updated terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,5 +9,8 @@ terraform {
 
 provider "kafka" {
   bootstrap_servers = ["localhost:9094"]
-  tls_enabled = false
+  tls_enabled = true
+  ca_cert = file("../truststore/kafka.truststore.pem")
+  client_cert = file("../keystore/kafka.keystore.pem")
+  client_key = file("../keystore/kafka.keystore.key")
 }
