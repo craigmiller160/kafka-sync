19aacb3b9085f7daf08e39bf9b02b9b5de236beb
Craig Miller <craigmiller160@gmail.com>
2024-01-01 12:29:56 -0500

    Adjustments

--- scripts/init.sh
+++ scripts/init.sh
@@ -11,6 +11,13 @@ terraform_apply() {
 }
 
 generate_tls() {
+  do_generate_tls=""
+  echo -n "Do you want to generate TLS certificates?"
+
   echo "Generating TLS certificates"
   (cd scripts && bash kafka-generate-ssl.sh && bash kafka-ssl-postprocess.sh)
 }
+
+docker_start
+terraform_apply
+generate_tls
