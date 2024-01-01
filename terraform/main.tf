9263c6a1ead5549187ea42e5b25078c9d348f77e
Craig Miller <craigmiller160@gmail.com>
2024-01-01 11:33:40 -0500

    Updating terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,5 +12,5 @@ provider "kafka" {
   tls_enabled = true
   ca_cert = file("../truststore/kafka.truststore.pem")
   client_cert = file("../keystore/kafka.keystore.pem")
-  client_key = file("../truststore/ca-key")
+  client_key = file("../truststore/ca-key.pem")
 }
