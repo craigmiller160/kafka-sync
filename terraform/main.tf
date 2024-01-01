77bde5f83245fa6984780cdd3ca8468fdf3f3708
Craig Miller <craigmiller160@gmail.com>
2024-01-01 14:51:22 -0500

    Trying to make things work

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,8 @@ provider "kafka" {
   bootstrap_servers = ["localhost:9094"]
   tls_enabled = true
   ca_cert = file("../certs/ca.cert.pem")
-  client_cert = file("../certs/localhost.cert.pem")
-  client_key = file("../certs/localhost.key.dec.pem")
+  client_cert = file("../certs/ca.cert.pem")
+  client_key = file("../certs/ca.key.dec.pem")
   skip_tls_verify = true
   sasl_mechanism = "plain"
   sasl_username = "user"
