75ec0fb59f89569476f6ae4cadac9d813701ae26
Craig Miller <craigmiller160@gmail.com>
2024-01-01 16:43:05 -0500

    Acl (#3)
    * Trying to add authorizer
    
    * Got the authorizer to work with the super user
    
    * More fixes for user config
    
    * Added acls
    
    * Fixing acl settings
    
    * Trying to get the UI working again
    
    * Turning internal sasl back on
    
    * Updated .gitignore settings
    
    * Fixing a few more settings

---
+++ terraform/acls.tf
@@ -0,0 +1,35 @@
+resource "kafka_acl" "producer_user_person_topic_acl" {
+  acl_host            = "*"
+  acl_operation       = "Write"
+  acl_permission_type = "Allow"
+  acl_principal       = "User:producer_user"
+  resource_name       = "person-topic"
+  resource_type       = "Topic"
+}
+
+resource "kafka_acl" "consumer_user_person_topic_acl" {
+  acl_host            = "*"
+  acl_operation       = "Read"
+  acl_permission_type = "Allow"
+  acl_principal       = "User:consumer_user"
+  resource_name       = "person-topic"
+  resource_type       = "Topic"
+}
+
+resource "kafka_acl" "consumer_user_person_topic_group_acl" {
+  acl_host            = "*"
+  acl_operation       = "Read"
+  acl_permission_type = "Allow"
+  acl_principal       = "User:consumer_user"
+  resource_name       = "person-topic-group"
+  resource_type       = "Group"
+}
+
+resource "kafka_acl" "consumer_user_person_topic_dlt_acl" {
+  acl_host            = "*"
+  acl_operation       = "Write"
+  acl_permission_type = "Allow"
+  acl_principal       = "User:consumer_user"
+  resource_name       = "person-topic-dlt"
+  resource_type       = "Topic"
+}
