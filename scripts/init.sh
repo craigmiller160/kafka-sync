fc0bd7a75e201b47f4f1febd43f4652b38a6b881
Craig Miller <craigmiller160@gmail.com>
2024-01-01 14:40:11 -0500

    Adding checks of command statuses

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,21 +3,25 @@
 docker_start() {
   echo "Starting docker applications"
   docker compose up -d
+  check_command_status $?
 }
 
 terraform_apply() {
   echo "Running terraform scripts"
   (cd terraform && terraform init -reconfigure && terraform apply)
+  check_command_status $?
 }
 
 generate_tls() {
   do_generate_tls=""
   echo -n "Do you want to generate TLS certificates? (y/n) "
   read -r do_generate_tls
+  check_command_status $?
 
   if [ "$do_generate_tls" == "y" ]; then
     echo "Generating TLS certificates"
     bash ./scripts/tls.sh
+    check_command_status $?
   fi
 }
 
